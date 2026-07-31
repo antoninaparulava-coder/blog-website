ae632c69cfd1ed4254a1615ee898114e8d79bd09
antoninaparulava-coder <antoninaparulava@gmail.com>
2026-07-31 19:50:59 +0400

    Getting Started With The Home Page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,5 +1,12 @@
+import { Fragment } from "react"
+
 function AllPostsPage(params) {
-    
+    return (
+        <Fragment>
+            <Hero />
+            <FeaturedPosts />
+        </Fragment>
+    )
 }
 
 export default AllPostsPage
