fd249fafab9418860be5beefe7132d0051fb897e
antoninaparulava-coder <antoninaparulava@gmail.com>
2026-08-01 18:35:53 +0400

    Adding The Hero Component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
+import { Fragment } from "react"
+
+import Hero from "@/components/home-page/hero"
+
 function HomePage(params) {
-    
+    return (
+        <Fragment>
+            <Hero />
+        </Fragment>
+    )
 }
 
 export default HomePage
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,12 +1,5 @@
-import { Fragment } from "react"
-
 function AllPostsPage(params) {
-    return (
-        <Fragment>
-            <Hero />
-            <FeaturedPosts />
-        </Fragment>
-    )
+    
 }
 
 export default AllPostsPage
