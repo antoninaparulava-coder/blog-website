9c128d8f0443d190f99a58fe9354de777533e505
antoninaparulava-coder <antoninaparulava@gmail.com>
2026-08-01 18:58:35 +0400

    Adding Layout & Navigation

---
+++ components/layout/layout.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+
+import MainNavigation from './main-navigation'
+
+function layout(props){
+    return (
+        <Fragment>
+            <MainNavigation />
+            <main>{props.children}</main>
+        </Fragment>
+    )
+}
+
+export default layout
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import "@/styles/globals.css";
+import layout from "@/components/layout/logo";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <layout>
+        <Component {...pageProps} />;
+    </layout>
+  )
 }
